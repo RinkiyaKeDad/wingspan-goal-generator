4469eb18b3a1c1bc1cac277a46935266c90b5ee8
Arsh Sharma <arshsharma461@gmail.com>
2025-06-15 17:36:28 +0530

    add oceania goals
    Signed-off-by: Arsh Sharma <arshsharma461@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,76 @@
-import { useState } from 'react';
-import './App.css'
-import Header from './Header'
-import Goal from './Goal';
-
+import { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./Header";
+import Goal from "./Goal";
 
 function App() {
-  const [arr, setArr] = useState(generateRandomNumbers())
+  const [oceania, setOceania] = useState(false);
+  const [arr, setArr] = useState([]);
 
-  function generateRandomNumbers() {
+  function generateRandomNumbers(oceania) {
     let set = new Set();
+    let len = oceania ? 12 : 8;
     while (set.size != 4) {
-      set.add(getRandomInt(8))
+      set.add(getRandomInt(len));
     }
-    let arr = [];
-    for (let num of set) {
-      arr.push(num)
-    }
-    return arr
+    return [...set].map((tile) => ({ tile, side: getRandomInt(2) }));
   }
 
-  return (
-    <div className='container'>
-      <Header />
-      <button onClick={() => setArr(generateRandomNumbers)}> Generate</button>
-      <div className='goal-table'>
-        {arr.map((val, index) => <Goal round={index} tile={val} side={getRandomInt(2)} />)}
-      </div>
-      <div className='para'> 
-        <a href='https://boardgamegeek.com/boardgame/266192/wingspan' target="_blank">Wingspan</a> is a card-driven engine-building board game made by Elizabeth Hargrave and Stonemaier Games. In the game the players assume the role of bird enthusiasts who are attract the best birds to their aviaries. The goal in the game is to score the most points by placing birds and laying eggs in different habitats.
+  // Initial generation
+  useEffect(() => {
+    setArr(generateRandomNumbers(oceania));
+  }, [oceania]); // Only run once on mount
 
-        The game plays over four rounds. This website helps you pick the end of round goals by selecting 4 out of the 16 available options for you at random.
+  return (
+    <div className="container">
+      <Header />
+      <button onClick={() => setArr(generateRandomNumbers(oceania))}>
+        Generate
+      </button>
+      <div>
+        <input
+          type="checkbox"
+          id="oceania"
+          name="oceania"
+          value="Oceania"
+          checked={oceania}
+          onChange={() => setOceania(!oceania)}
+        />
+        Oceania
+      </div>
+
+      <div className="goal-table">
+        {arr.map((goal, index) => (
+          <Goal
+            key={index}
+            oceania={oceania}
+            round={index}
+            tile={goal.tile}
+            side={goal.side}
+          />
+        ))}
+      </div>
+      <div className="para">
+        <a
+          href="https://boardgamegeek.com/boardgame/266192/wingspan"
+          target="_blank"
+        >
+          Wingspan
+        </a>{" "}
+        is a card-driven engine-building board game made by Elizabeth Hargrave
+        and Stonemaier Games. In the game the players assume the role of bird
+        enthusiasts who are attract the best birds to their aviaries. The goal
+        in the game is to score the most points by placing birds and laying eggs
+        in different habitats. The game plays over four rounds. This website
+        helps you pick the end of round goals by selecting 4 out of the 16
+        available options for you at random.
       </div>
     </div>
-  )
+  );
 }
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-export default App
+export default App;
--- src/Goal.jsx
+++ src/Goal.jsx
@@ -1,38 +1,61 @@
-import './Goal.css'
+import "./Goal.css";
 
-import goal00 from './assets/goal-00.jpg';
-import goal01 from './assets/goal-01.jpg';
-import goal10 from './assets/goal-10.jpg';
-import goal11 from './assets/goal-11.jpg';
-import goal20 from './assets/goal-20.jpg';
-import goal21 from './assets/goal-21.jpg';
-import goal30 from './assets/goal-30.jpg';
-import goal31 from './assets/goal-31.jpg';
-import goal40 from './assets/goal-40.jpg';
-import goal41 from './assets/goal-41.jpg';
-import goal50 from './assets/goal-50.jpg';
-import goal51 from './assets/goal-51.jpg';
-import goal60 from './assets/goal-60.jpg';
-import goal61 from './assets/goal-61.jpg';
-import goal70 from './assets/goal-70.jpg';
-import goal71 from './assets/goal-71.jpg';
+import goal00 from "./assets/goal-00.jpg";
+import goal01 from "./assets/goal-01.jpg";
+import goal10 from "./assets/goal-10.jpg";
+import goal11 from "./assets/goal-11.jpg";
+import goal20 from "./assets/goal-20.jpg";
+import goal21 from "./assets/goal-21.jpg";
+import goal30 from "./assets/goal-30.jpg";
+import goal31 from "./assets/goal-31.jpg";
+import goal40 from "./assets/goal-40.jpg";
+import goal41 from "./assets/goal-41.jpg";
+import goal50 from "./assets/goal-50.jpg";
+import goal51 from "./assets/goal-51.jpg";
+import goal60 from "./assets/goal-60.jpg";
+import goal61 from "./assets/goal-61.jpg";
+import goal70 from "./assets/goal-70.jpg";
+import goal71 from "./assets/goal-71.jpg";
+import goalOc00 from "./assets/goal-oc-00.jpeg";
+import goalOc01 from "./assets/goal-oc-01.jpeg";
+import goalOc10 from "./assets/goal-oc-10.jpeg";
+import goalOc11 from "./assets/goal-oc-11.jpeg";
+import goalOc20 from "./assets/goal-oc-20.jpeg";
+import goalOc21 from "./assets/goal-oc-21.jpeg";
+import goalOc30 from "./assets/goal-oc-30.jpeg";
+import goalOc31 from "./assets/goal-oc-31.jpeg";
 
-function Goal({round ,tile, side }) {
-    let goals = [
-        [goal00, goal01],
-        [goal10, goal11],
-        [goal20, goal21],
-        [goal30, goal31],
-        [goal40, goal41],
-        [goal50, goal51],
-        [goal60, goal61],
-        [goal70, goal71]
-    ];
-    return (
-        <div key={round+1} className='goal'>
-            <img src={goals[tile][side]} alt={`Round ${round+1} Goal`} />
-        </div>
-    )
+function Goal({ oceania, round, tile, side }) {
+  let goals = [
+    [goal00, goal01],
+    [goal10, goal11],
+    [goal20, goal21],
+    [goal30, goal31],
+    [goal40, goal41],
+    [goal50, goal51],
+    [goal60, goal61],
+    [goal70, goal71],
+  ];
+  if (oceania) {
+    goals.push(
+      [goalOc00, goalOc01],
+      [goalOc10, goalOc11],
+      [goalOc20, goalOc21],
+      [goalOc30, goalOc31]
+    );
+  }
+
+  // Defensive check
+  if (!goals[tile]) {
+    console.warn(`Invalid tile index: ${tile}`);
+    return null; // or a placeholder
+  }
+
+  return (
+    <div key={round + 1} className="goal">
+      <img src={goals[tile][side]} alt={`Round ${round + 1} Goal`} />
+    </div>
+  );
 }
 
-export default Goal
+export default Goal;
